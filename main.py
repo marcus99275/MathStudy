25c0a2cfbec36e496ecb6af9f1e5aa1b1df569a5
Marcus99275 <s0710749.kh@go.edu.tw>
2024-03-15 13:21:53 +0800

    a bunch of major algorithm change (y im being lazy now)

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-import sys
 from time import time
 
 import drawsvg as svg
@@ -11,18 +9,15 @@ import customobject as custom
 custom.init_result_storage()
 
 # make an option menu to choose actions
-print('Options: (1)Create lands (2)Build lands from .landseq file [NOT COMPLETED] (3)Exit: ', end='')
+print('Choose land type: (1)Square (2)Rectangle: ', end='')
 option = int(input())
-if option == 1:
-    pass
-elif option == 3:
-    sys.exit(0)
-else:
-    raise ValueError('Option not defined or not implemented')
-print('Choose land type: (1)Square (2)Rectangle (3)Parallelogram (4)Circle: ', end='')
-option = int(input())
-switch_1 = {1: lambda a: 'square', 2: lambda a: 'rectangle', 3: lambda a: '', 4: lambda a: 'circle'}
+switch_1 = {1: lambda a: 'square', 2: lambda a: 'rectangle'}
 land_type = switch_1[option](0)
+
+# option menu for entering number of entry
+print('Input entry count(1 or 2): ', end='')
+option = int(input())
+entry = option
 
 # get size of land
 if option == 1:
@@ -30,18 +25,12 @@ if option == 1:
     width = int(input()) * 10
     height = width
     surface = height ** 2
-elif option == 2 or option == 3:
+elif option == 2:
     print('enter land width (mult by 10):', end=' ')
     width = int(input())*10
     print('enter land height (mult by 10):', end=' ')
     height = int(input())*10
     surface = width * height
-elif option == 4:
-    print('enter land radius (mult by 10):', end=' ')
-    radius = int(input())*10
-    surface = round(radius ** math.pi, 2)
-    width = round(radius * math.sqrt(2), 2)
-    height = width
 else:
     width = 10
     height = 10
@@ -64,7 +53,7 @@ drawObj.append(r)
 
 # drawing x grid on right side
 grid_seq_1 = 0
-for i in trange(int(height/10*4)):
+for i in trange(int(height/c.grid_x_height)):
     r = custom.ParkingGrid(x=width - c.grid_x_width, y=grid_seq_1, width=c.grid_x_width, height=c.grid_x_height)
     drawObj.append(r)
     grid_seq_1 = grid_seq_1 + c.grid_x_height
@@ -72,30 +61,30 @@ for i in trange(int(height/10*4)):
 
 # drawing y grid on left side if necessary
 grid_seq_2 = 0
-grid_seq_3 = (width - 9) % 2.5
-path_y_axis = 5.5
+grid_seq_3 = (width - 9) % c.grid_y_width
+path_y_axis = c.grid_y_height
 if height > 10 and width > 10:
     for i in trange(int(height/9)):
-        for j in range(int((width - 8) / 2.5)):
+        for j in range(int((width - 8) / c.grid_y_width)):
             r = custom.ParkingGrid(x=grid_seq_3, y=grid_seq_2, width=c.grid_y_width, height=c.grid_y_height)
             drawObj.append(r)
-            grid_seq_3 += 2.5
+            grid_seq_3 += c.grid_y_width
             grid_count += 1
         r = svg.Rectangle(x=0, y=path_y_axis, height=c.path_width, width=width - 9, fill=c.color_path, stroke=c.stroke, stroke_width=c.stroke_width)
         drawObj.append(r)
-        grid_seq_3 = (width - 9) % 2.5
+        grid_seq_3 = (width - 9) % c.grid_y_width
         grid_seq_2 += 9
         path_y_axis += 9
 
 # draw auxiliary grid to fill space
 grid_seq_4 = 0
-if height % 9 > 5.5:
-    for i in trange(int((width - 9)/2.5)):
+if height % 9 > c.grid_y_height:
+    for i in trange(int((width - 9)/c.grid_x_height)):
         r = custom.ParkingGrid(x=width - (11.5 + grid_seq_4 * 2.5), y=path_y_axis - 5.5, height=c.grid_y_height, width=c.grid_y_width)
         drawObj.append(r)
         grid_seq_4 += 1
-elif height % 9 > 2.5:
-    for i in trange(int((width - 9)/5.5)):
+elif height % 9 > c.grid_x_height:
+    for i in trange(int((width - 9)/c.grid_y_height)):
         r = custom.ParkingGrid(x=width - (14.5 + grid_seq_4 * 5.5), y=path_y_axis - 5.5, height=c.grid_x_height, width=c.grid_x_width)
         drawObj.append(r)
         grid_seq_4 += 1
@@ -111,8 +100,9 @@ if width == 10 and height == 10:
     r = svg.Rectangle(x=width - c.grid_x_width - c.path_width, y=0, width=c.path_width, height=1, fill=c.color_entry,
                       stroke=c.stroke, stroke_width=c.stroke_width)
     drawObj.append(r)
-    r = svg.Rectangle(x=width - c.grid_x_width - c.path_width, y=height - 1, width=c.path_width, height=1, fill=c.color_entry,
-                      stroke=c.stroke, stroke_width=c.stroke_width)
+    if entry > 1:
+        r = svg.Rectangle(x=width - c.grid_x_width - c.path_width, y=height - 1, width=c.path_width, height=1, fill=c.color_entry,
+                          stroke=c.stroke, stroke_width=c.stroke_width)
     drawObj.append(r)
     drawObj.save_svg("result/square/10x10.svg")
     print(f'Grid Counts: {grid_count}')
@@ -120,13 +110,11 @@ elif width >= 10 and height >= 10:
     r = svg.Rectangle(x=0, y=5.5, width=1, height=c.path_width, fill=c.color_entry,
                       stroke=c.stroke, stroke_width=c.stroke_width)
     drawObj.append(r)
-    r = svg.Rectangle(x=0, y=height - (height % 9) - 3.5, width=1, height=c.path_width, fill=c.color_entry,
-                      stroke=c.stroke, stroke_width=c.stroke_width)
+    if entry > 1:
+        r = svg.Rectangle(x=0, y=height - (height % 9) - 3.5, width=1, height=c.path_width, fill=c.color_entry,
+                          stroke=c.stroke, stroke_width=c.stroke_width)
     drawObj.append(r)
-    if land_type == 'circle':
-        drawObj.save_svg(f'result/{land_type}/{surface}.svg')
-    else:
-        drawObj.save_svg(f'result/{land_type}/{width}x{height}.svg')
+    drawObj.save_svg(f'result/{land_type}/{width}x{height}.svg')
     print(f'Grid Count: {grid_count}')
 
 print(f'Spends {time() - s_time} seconds.')
